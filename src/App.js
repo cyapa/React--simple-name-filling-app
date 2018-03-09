145a25cc34e59f6b1876c6fac8c8cda7b8b603b7
cyapa <kymcwyapa@gmail.com>
2018-03-09 23:36:50 +0200

    initial checkin
    Simple name filling form

--- src/App.js
+++ src/App.js
@@ -2,20 +2,107 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
+import RaisedButton from 'material-ui/RaisedButton'
+import { grey200, grey50, grey600, grey300, green100, white, green600 } from 'material-ui/styles/colors';
+
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      fname: '',
+      mName: '',
+      lName: '',
+      midNameDisability:true
+    }
+  }
+  autoFillClicked = ()=>{
+
+    this.setState(
+      {
+        fname:'Chathura',
+        mName:'Wijebandara',
+        lName:'Yapa',
+        midNameDisability: true
+
+      }
+    );
+  }
+
+  setMnameAvailability = ()=>{
+    let avail = !this.state.midNameDisability
+    this.setState(
+      {
+        midNameDisability: avail
+      }
+    );
+  }
+  disableMname = (checked) =>{
+    this.setState(
+      {
+        fname:this.state.fname,
+        mName:this.state.mName,
+        lName:this.state.lName,
+        midNameDisability:false
+      }
+    );
+
+  }
+
+  fNameChanged = (event,input)=>{
+
+    this.setState(
+      {
+        fname:input
+      }
+    );
+  }
+
+  mNameChanged = (event,input)=>{
+
+    this.setState(
+      {
+        mName:input
+      }
+    );
+  }
+
+  lNameChanged = (event,input)=>{
+
+    this.setState(
+      {
+        lName:input
+      }
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <MuiThemeProvider>
+          <Paper style={{width:600,height:350,backgroundColor:green100}}>
+            <div style={{float:'left', width:'100%'}}>
+            First Name: <TextField id='f-name-txt' style={{backgroundColor:white}} defaultValue='h' value={this.state.fname} onChange={(e,v) =>{this.fNameChanged(v)}}/>
+            </div>
+            <div style={{float:'left', width:'100%'}}>
+            Middle Name: <TextField id='m-name-txt' style={{backgroundColor:white}} value={this.state.mName} onChange={(e,v) =>{this.mNameChanged(v)}} disabled={this.state.midNameDisability}/>
+            </div>
+            <div style={{float:'left', width:'100%'}}>
+            Last Name: <TextField id='l-name-txt' style={{backgroundColor:white}} value={this.state.lName} onChange={(e,v) =>{this.lNameChanged(v)}}/>
+            </div>
+            <div style={{float:'left', width:'100%'}}>
+              Middle Name: <Checkbox id='m-name-ckbx' onClick={(e,v)=>{this.setMnameAvailability()}}/> 
+            </div>
+            <RaisedButton id='autofill-btn' label='Autofill' style={{float:'center', width:'100%',fontWeight:'bold',backgroundColor:grey600}} onClick={(e)=>{this.autoFillClicked();}}/>
+          </Paper>
+        </MuiThemeProvider>
     );
   }
 }
 
 export default App;
+// onCheck={(e,v)=>{this.disableMname(v);}}  />
